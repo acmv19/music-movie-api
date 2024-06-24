038f6fd85d125518cdd6cdf2657d8a9a06248fe9
mava1 <anamariamaldonado1994@gmail.com>
2024-06-24 11:09:03 -0400

    final proyect part 2

--- public/index.js
+++ public/index.js
@@ -18,8 +18,8 @@ export const enableInput = (state) => {
   inputEnabled = state;
 };
 
-export let token = null;
-
+export let token = null; // Variable to store user authentication token
+// Function to set the authentication token
 export const setToken = (value) => {
   token = value;
   if (value) {
@@ -31,7 +31,7 @@ export const setToken = (value) => {
   }
 };
 
-export let message = null;
+export let message = null; // Variable to store message element
 
 import { showMusic, handleMusic } from "./music.js";
 import { showMovie, handleMovie } from "./movie.js";
@@ -52,8 +52,69 @@ document.addEventListener("DOMContentLoaded", () => {
   handleAddEdit();
   handleAddEditM();
   setToken(token); // Llama a setToken para configurar la vista inicial basada en el token
+  // <<<<----------Agregar manejadores de eventos para los botones de búsqueda
+  document
+    .getElementById("music-search")
+    .addEventListener("keyup", SearchMusic); // keyup:detectar la acción de soltar una tecla y se utiliza comúnmente para realizar acciones inmediatas basadas en texto ingresado por user
+  document
+    .getElementById("movie-search")
+    .addEventListener("keyup", searchMovie);
+  //<<<<<<<----------------------------------
 });
+//<<<<<----------buscadores----------->>>>>>>
+//MUSIC
+function SearchMusic() {
+  const table = document.getElementById("music-table");
+  const searchTerm = document
+    .getElementById("music-search")
+    .value.toLowerCase();
 
+  for (let i = 1; i < table.rows.length; i++) {
+    // filas
+    let row = table.rows[i];
+    let found = false;
+    const cells = row.getElementsByTagName("td");
+
+    for (let j = 0; j < cells.length; j++) {
+      //celdas
+      const cellContent = cells[j].innerHTML.toLowerCase();
+      if (cellContent.indexOf(searchTerm) > -1) {
+        found = true;
+        break;
+      }
+    }
+
+    row.style.display = found ? "" : "none"; // mostrar
+  }
+}
+//MOVIE
+function searchMovie() {
+  const table = document.getElementById("movie-table");
+  const searchTerm = document
+    .getElementById("movie-search")
+    .value.toLowerCase();
+
+  for (let i = 1; i < table.rows.length; i++) {
+    // filas
+    let row = table.rows[i];
+    let found = false;
+    const cells = row.getElementsByTagName("td");
+
+    for (let j = 0; j < cells.length; j++) {
+      //celdas
+      const cellContent = cells[j].innerHTML.toLowerCase();
+      if (cellContent.indexOf(searchTerm) > -1) {
+        // probar conrenido = al que se busca
+        found = true; // found coincidencia
+        break; // No es necesario seguir buscando en esta fila
+      }
+    }
+
+    row.style.display = found ? "" : "none"; // mostrar
+  }
+}
+
+//<<<<<<<<<----------------
 function showNavigation() {
   const navigationDiv = document.getElementById("navigation");
   navigationDiv.style.display = "block";
@@ -77,8 +138,8 @@ function showMusicTable() {
   const musicDiv = document.getElementById("music");
   const movieDiv = document.getElementById("movie");
 
-  musicDiv.style.display = "block";
-  movieDiv.style.display = "none";
+  musicDiv.style.display = "block"; //mostrar
+  movieDiv.style.display = "none"; //ocultar
 
   showMusic(); // Llamar a la función para mostrar la tabla de música
 }
